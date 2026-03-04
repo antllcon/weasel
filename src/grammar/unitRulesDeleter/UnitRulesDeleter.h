7b28b203805e542d226de2255a6ee9fae7222d51
antlllcon <pokemonivan32@gmail.com>
2026-03-04 14:06:54 +0300

    [main] Реализовал удаление цепных правил

--- src/grammar/unitRulesDeleter/UnitRulesDeleter.h
+++ src/grammar/unitRulesDeleter/UnitRulesDeleter.h
@@ -1,11 +1,10 @@
 #pragma once
-#include "GrammarTypes.h"
+#include "src/grammar/GrammarTypes.h"
 
 class UnitRulesDeleter
 {
 public:
 	explicit UnitRulesDeleter(raw::Rules rules);
-
 	[[nodiscard]] raw::Rules DeleteUnitRules();
 
 private:

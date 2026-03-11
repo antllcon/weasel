cde5941b20d58bb72d14359c076d867fe413da61
antlllcon <pokemonivan32@gmail.com>
2026-03-11 11:54:36 +0300

    [main] Доработал вывод

--- src/grammar/printGrammar/PrintGrammar.cpp
+++ src/grammar/printGrammar/PrintGrammar.cpp
@@ -1,6 +1,13 @@
 #include "PrintGrammar.h"
-
 #include <iostream>
+
+namespace
+{
+bool IsFlagActive(OptimizationFlags value, OptimizationFlags flag)
+{
+	return (static_cast<uint32_t>(value) & static_cast<uint32_t>(flag)) != 0;
+}
+} // namespace
 
 raw::Rule PrintGrammar::MakeRule(const std::string& name, const raw::Alternatives& alts)
 {
@@ -71,5 +78,46 @@ void PrintGrammar::PrintRulesWithGuides(const Rules& rules, const std::string& t
 		}
 		std::cout << std::endl;
 	}
+	std::cout << std::endl;
+}
+
+void PrintGrammar::PrintAppliedOptimizations(OptimizationFlags flags)
+{
+	if (flags == OptimizationFlags::None)
+	{
+		std::cout << "  - Исходная грамматика уже является LL(1) (Без оптимизаций)" << std::endl;
+		return;
+	}
+
+	if (IsFlagActive(flags, OptimizationFlags::LeftFactorize))
+	{
+		std::cout << "  - Левая факторизация" << std::endl;
+	}
+
+	if (IsFlagActive(flags, OptimizationFlags::EliminateLeftRecursion))
+	{
+		std::cout << "  - Устранение левой рекурсии" << std::endl;
+	}
+
+	if (IsFlagActive(flags, OptimizationFlags::FilterUnreachable))
+	{
+		std::cout << "  - Удаление недостижимых правил" << std::endl;
+	}
+
+	if (IsFlagActive(flags, OptimizationFlags::FilterUnproductive))
+	{
+		std::cout << "  - Удаление непродуктивных правил" << std::endl;
+	}
+
+	if (IsFlagActive(flags, OptimizationFlags::DeleteUnitRules))
+	{
+		std::cout << "  - Удаление цепных правил" << std::endl;
+	}
+
+	if (IsFlagActive(flags, OptimizationFlags::DeleteEmptyRules))
+	{
+		std::cout << "  - Удаление пустых правил" << std::endl;
+	}
+
 	std::cout << std::endl;
 }
--- src/grammar/printGrammar/PrintGrammar.h
+++ src/grammar/printGrammar/PrintGrammar.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "src/grammar/GrammarTypes.h"
+#include "src/grammar/grammarOptimizer/GrammarOptimizer.h"
+#include <iostream>
 
 namespace PrintGrammar
 {
 raw::Rule MakeRule(const std::string& name, const raw::Alternatives& alts);
 void PrintRules(const raw::Rules& rules, const std::string& title = "");
 void PrintRulesWithGuides(const Rules& rules, const std::string& title = "");
-
+void PrintAppliedOptimizations(OptimizationFlags flags);
 } // namespace PrintGrammar

c6a799edcfcc85df0fffea1352c0325c3ec5fc13
antlllcon <pokemonivan32@gmail.com>
2026-04-01 16:26:57 +0300

    [main] Исправил представление Chunk'а

--- src/vm/chank/Chunk.cpp
+++ src/vm/chank/Chunk.cpp
@@ -1,5 +1,4 @@
 #include "Chunk.h"
-
 #include <stdexcept>
 
 namespace
@@ -11,23 +10,48 @@ void AssertIsConstantIndexValid(size_t size)
 		throw std::runtime_error("Превышен лимит констант в одном блоке (максимум 256)");
 	}
 }
-} // namespace
 
-void Chunk::WriteByte(uint8_t byte)
+void AssertIsStringIndexValid(size_t size)
 {
-	m_code.push_back(byte);
+	if (size > 255)
+	{
+		throw std::runtime_error("Превышен лимит строк в одном блоке (максимум 256)");
+	}
 }
 
-void Chunk::WriteOpCode(OpCode code)
+void AssertIsOffsetValid(bool isValid)
+{
+	if (!isValid)
+	{
+		throw std::runtime_error("Смещение инструкции выходит за пределы массива строк кода");
+	}
+}
+}
+
+void Chunk::WriteByte(uint8_t byte, uint32_t line)
+{
+	m_code.push_back(byte);
+	m_lines.push_back(line);
+}
+
+void Chunk::WriteOpCode(OpCode code, uint32_t line)
 {
 	m_code.push_back(static_cast<uint8_t>(code));
+	m_lines.push_back(line);
 }
 
 uint8_t Chunk::AddConstant(Value value)
 {
 	AssertIsConstantIndexValid(m_constants.size());
-	m_constants.push_back(std::move(value));
+	m_constants.push_back(value);
 	return static_cast<uint8_t>(m_constants.size() - 1);
+}
+
+uint8_t Chunk::AddString(const std::string& text)
+{
+	AssertIsStringIndexValid(m_strings.size());
+	m_strings.push_back(text);
+	return static_cast<uint8_t>(m_strings.size() - 1);
 }
 
 const std::vector<uint8_t>& Chunk::GetCode() const
@@ -38,4 +62,15 @@ const std::vector<uint8_t>& Chunk::GetCode() const
 const std::vector<Value>& Chunk::GetConstants() const
 {
 	return m_constants;
+}
+
+const std::vector<std::string>& Chunk::GetStrings() const
+{
+	return m_strings;
+}
+
+uint32_t Chunk::GetLine(uint32_t instructionOffset) const
+{
+	AssertIsOffsetValid(instructionOffset < m_lines.size());
+	return m_lines[instructionOffset];
 }
--- src/vm/chank/Chunk.h
+++ src/vm/chank/Chunk.h
@@ -2,19 +2,25 @@
 #include "src/vm/OpCode.h"
 #include "src/vm/value/Value.h"
 #include <cstdint>
+#include <string>
 #include <vector>
 
 class Chunk
 {
 public:
-	void WriteByte(uint8_t byte);
-	void WriteOpCode(OpCode code);
+	void WriteByte(uint8_t byte, uint32_t line);
+	void WriteOpCode(OpCode code, uint32_t line);
 	uint8_t AddConstant(Value value);
+	uint8_t AddString(const std::string& text);
 
 	const std::vector<uint8_t>& GetCode() const;
 	const std::vector<Value>& GetConstants() const;
+	const std::vector<std::string>& GetStrings() const;
+	uint32_t GetLine(uint32_t instructionOffset) const;
 
 private:
 	std::vector<uint8_t> m_code;
+	std::vector<uint32_t> m_lines;
 	std::vector<Value> m_constants;
+	std::vector<std::string> m_strings;
 };
